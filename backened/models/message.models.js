7067da0b68da8a48439fab3d6bcc5537c60b1eca
morty07-os <amin19osmani@gmail.com>
2025-05-18 19:49:24 +0100

    conversation works(sending)

--- backened/models/message.models.js
+++ backened/models/message.models.js
@@ -1,27 +1,36 @@
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
-  conversationId: {
+  carId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Conversation',
-    required: true,
+    ref: 'Car',
+    required: true
   },
   sender: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
+    required: true
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  conversationId: {
+    type: String,
+    required: true
   },
   text: {
     type: String,
-    required: true,
+    required: true
   },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-}, {
-  timestamps: true,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+messageSchema.index({ carId: 1, conversationId: 1 });
 
 const Message = mongoose.model('Message', messageSchema);
 
