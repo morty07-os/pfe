e20b977398f821fea6d25f8388e52cfde1373522
ososos999 <hachembouzahzah67@gmail.com>
2025-05-20 07:15:43 +0200

    Resolved merge conflict by removing MapSelectPage and keeping conversation/:carId/:ownerId route

--- car-rental-website/src/App.js
+++ car-rental-website/src/App.js
@@ -9,22 +9,18 @@ import MapSection from './components/MapSection';
 
 import AllOffersPage from './pages/AllOffersPage';
 import OfferDetailsPage from './pages/OfferDetailsPage';
-// import ChatPage from './pages/ChatPage'; // Will be replaced by the new ChatPage import
 import ProfilePage from './pages/ProfilePage';
-import CarDetailsPage from './pages/CarDetailsPage'; // Import the CarDetailsPage component
-import BookingPage from './pages/BookingPage'; // Import the BookingPage component
-// MapSelectPage has been removed
+import CarDetailsPage from './pages/CarDetailsPage';
+import BookingPage from './pages/BookingPage';
 import ConversationPage from './pages/ConversationPage';
 import ConversationListPage from './pages/ConversationListPage';
-// Import new pages
 import CategoriesPage from './pages/CategoriesPage';
 import DealsPage from './pages/DealsPage';
 import AboutPage from './pages/AboutPage';
 import FaqPage from './pages/FaqPage';
 import ContactPage from './pages/ContactPage';
 import ReviewsPage from './pages/ReviewsPage';
-import AddCarPage from './pages/AddCarPage'; // Import the new AddCarPage
-
+import AddCarPage from './pages/AddCarPage';
 
 function App() {
   return (
@@ -43,21 +39,18 @@ function App() {
 
             <Route path="/offers" element={<AllOffersPage />} />
             <Route path="/offer/:offerId" element={<OfferDetailsPage />} />
-            {/* <Route path="/chat/:posterName" element={<ChatPage />} /> */} {/* Old chat route, to be updated or removed */}
             <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/car-details/:carId" element={<CarDetailsPage />} /> {/* Add route for CarDetailsPage */}
-            <Route path="/booking/:carId" element={<BookingPage />} /> {/* Add route for BookingPage */}
-            {/* MapSelectPage route removed */}
+            <Route path="/car-details/:carId" element={<CarDetailsPage />} />
+            <Route path="/booking/:carId" element={<BookingPage />} />
             <Route path="/conversation/:carId/:ownerId" element={<ConversationPage />} />
-            <Route path="/messages" element={<ConversationListPage />} /> {/* Add route for ConversationListPage */}
-            {/* New Routes */}
+            <Route path="/messages" element={<ConversationListPage />} />
             <Route path="/categories" element={<CategoriesPage />} />
             <Route path="/deals" element={<DealsPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/faq" element={<FaqPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/reviews" element={<ReviewsPage />} />
-            <Route path="/add-car" element={<AddCarPage />} /> {/* Add route for AddCarPage */}
+            <Route path="/add-car" element={<AddCarPage />} />
           </Routes>
         </div>
       </BrowserRouter>
