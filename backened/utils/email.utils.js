524784eafc8216547814e6ce263000b4677823fd
morty07-os <amin19osmani@gmail.com>
2025-05-28 18:34:50 +0100

    38

--- backened/utils/email.utils.js
+++ backened/utils/email.utils.js
@@ -7,8 +7,9 @@ const sendVerificationEmail = async (email, verificationCode) => {
     try {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+            port: 587,
+            secure: false, // Use 'false' for port 587
+            requireTLS: true, // Enforce TLS for port 587
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
