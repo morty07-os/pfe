0238d016363d9ebfd05b2d10f1f8627de3f58536
morty07-os <amin19osmani@gmail.com>
2025-05-29 23:27:09 +0100

    61

--- backened/models/car.models.js
+++ backened/models/car.models.js
@@ -76,9 +76,9 @@ const carSchema = new mongoose.Schema(
             required: true,
             validate: {
                 validator: function (images) {
-                    return images.length >= 1 && images.length <= 5 && images.every((img) => img.startsWith('uploads/'));
+                    return images.length >= 1 && images.length <= 5 && images.every((img) => img.startsWith('https://res.cloudinary.com/'));
                 },
-                message: 'You must upload between 1 and 5 images, and all image paths must start with "uploads/".',
+                message: 'You must upload between 1 and 5 images, and all image paths must start with "https://res.cloudinary.com/".',
             },
         },
         owner: {
