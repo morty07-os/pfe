c9c0582059ed4ac38009d50ec5ccea3fac190e5b
morty07-os <amin19osmani@gmail.com>
2025-05-31 18:21:07 +0100

    141

--- backened/controllers/admin.controller.js
+++ backened/controllers/admin.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.models.js';
+import sendEmail from '../utils/email.utils.js'; // Import sendEmail utility
 
 // Get all users with status 'pending'
 export const getPendingUsers = async (req, res) => {
@@ -29,8 +30,22 @@ export const approveUser = async (req, res) => {
         user.status = 'approved';
         await user.save();
 
-        // Optionally send an email to the user informing them of approval
-        // await sendEmail(user.email, 'Account Approved', 'Your account has been approved...');
+        // Send an email to the user informing them of approval
+        try {
+            await sendEmail(user.email, 'Account Approved', `
+                <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+                    <h2 style="color: #475569;">Account Approved</h2>
+                    <p>Good news! Your account on the Car Rental Website has been approved by the administrator.</p>
+                    <p>You can now log in to your account and start using all the features.</p>
+                    <p>Click here to log in: [Link to your login page]</p>
+                    <p>Best regards,<br>The Car Rental Team</p>
+                </div>
+            `);
+            console.log(`Approval email sent to: ${user.email}`);
+        } catch (emailError) {
+            console.error(`Error sending approval email to ${user.email}:`, emailError);
+            // Log the error but don't fail the approval process
+        }
 
         res.status(200).json({ message: "User approved successfully", userId: user._id });
     } catch (error) {
